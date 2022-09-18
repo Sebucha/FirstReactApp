3071a0747f62d76d7090a8ee647ce2ac17833dd6
Lukasz Formela <lukasz.formela@hpe.com>
2022-09-19 01:09:40 +0200

    feat(countdown): fixed the issue and styled the app on the way

--- src/components/CountdownUtils.js
+++ src/components/CountdownUtils.js
@@ -1,46 +1,23 @@
 import dayjs from "dayjs";
 
-export function getRemainingTimeUntilMsTimestamp(timestampMs) {
-    const timestampDayjs = dayjs(timestampMs);
-    const nowDayjs = dayjs();
-    if (timestampDayjs.isBefore(nowDayjs)) {
-        return {
-            seconds: '00',
-            minutes: '00',
-            hours: '00',
-            days: '00'
-        }
-    }
-    return {
-        seconds: getRemainingSeconds(nowDayjs, timestampDayjs),
-        minutes: getRemainingMinutes(nowDayjs, timestampDayjs),
-        hours: getRemainingHours(nowDayjs, timestampDayjs),
-        days: getRemainingDays(nowDayjs, timestampDayjs)
-    };
-}
+export const getRemainingTimeUntilMsTimestamp = (timestampMs) => {
+  const timestampDayjs = dayjs(timestampMs);
+  const nowDayjs = dayjs();
 
-function getRemainingSeconds(nowDayjs, timestampDayjs) {
-    const seconds = timestampDayjs.diff(nowDayjs, 'seconds') % 60;
-    return padWithZeros(seconds, 2);
-}
-
-function getRemainingMinutes(nowDayjs, timestampDayjs) {
-    const minutes = timestampDayjs.diff(nowDayjs, 'minutes') % 60;
-    return padWithZeros(minutes, 2);
-}
-
-function getRemainingHours(nowDayjs, timestampDayjs) {
-    const hours = timestampDayjs.diff(nowDayjs, 'hours') % 24;
-    return padWithZeros(hours, 2);
-}
-
-function getRemainingDays(nowDayjs, timestampDayjs) {
-    const days = timestampDayjs.diff(nowDayjs, 'days');
-    return days.toString();
-}
-
-function padWithZeros(number, minLength) {
+  const padWithZeros = (number, minLength) => {
     const numberString = number.toString();
-    if (numberString.length >= minLength) return numberString;
-    return "0".repeat(minLength - numberString.length) + numberString;
+    return (numberString.length >= minLength) ? numberString : "0".repeat(minLength - numberString.length) + numberString;
+  }
+
+  return timestampDayjs.isBefore(nowDayjs) ? {
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  } : {
+    days: timestampDayjs.diff(nowDayjs, 'days').toString(),
+    hours: padWithZeros((timestampDayjs.diff(nowDayjs, 'hours') % 24), 2),
+    minutes: padWithZeros((timestampDayjs.diff(nowDayjs, 'minutes') % 60), 2),
+    seconds: padWithZeros((timestampDayjs.diff(nowDayjs, 'seconds') % 60), 2),
+  };
 }
